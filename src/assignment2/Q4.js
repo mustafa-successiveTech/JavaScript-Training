7e0a0346327208bcbc5a9ffdad499b2daa0860bb
Md Mustafa Ali <md.ali@mustafass435.successivetech.com>
2025-06-04 16:49:44 +0530

    Assignment2 completed

---
+++ src/assignment2/Q4.js
@@ -0,0 +1,9 @@
+
+/*4. Write a function expression that takes in a number and returns its square.  */
+
+
+function getSquare(number){
+    return number*number;
+}
+
+console.log(getSquare(6));
---
+++ src/assignment2/Q5.js
@@ -0,0 +1,7 @@
+/* 5. Write a function expression that takes in two numbers and returns their sum.  */
+
+function getSum(a,b){
+    return a+b;
+}
+
+console.log(getSum(2,3));
