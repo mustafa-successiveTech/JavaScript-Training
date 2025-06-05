be6c15136f7f5515fc4d290cecf01483a5b8ff9b
Md Mustafa Ali <md.ali@mustafass435.successivetech.com>
2025-06-05 22:31:27 +0530

    Assignment-2 Updated

---
+++ src/assignment2/question2.js
@@ -0,0 +1,14 @@
+/*2. Write a function to take input(age, name, phone no.) from user and display it */
+
+function getUserInput() {
+  const age = prompt("Enter your age:");
+  const name = prompt("Enter your name:");
+  const phone = prompt("Enter your phone number:");
+
+  console.log("Age:", age);
+  console.log("Name:", name);
+  console.log("Phone Number:", phone);
+}
+    
+getUserInput();
+  
---
+++ src/assignment2/question3.js
@@ -0,0 +1,7 @@
+/*3. Write a function expression that takes in another function as an argument */
+
+const firstFunction = function(func) {
+  func();
+};
+    
+firstFunction(() => console.log("Function passed as argument!"));
