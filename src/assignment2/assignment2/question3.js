3e828c95f2e559f678693874d5dd09bfe782eba6
Md Mustafa Ali <md.ali@mustafass435.successivetech.com>
2025-06-05 22:33:17 +0530

    Assignment-2 Updated

--- src/assignment2/assignment2/question3.js
+++
@@ -1,7 +0,0 @@
-/*3. Write a function expression that takes in another function as an argument */
-
-const firstFunction = function(func) {
-  func();
-};
-    
-firstFunction(() => console.log("Function passed as argument!"));
