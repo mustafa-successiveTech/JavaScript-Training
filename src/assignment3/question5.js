ea1c52979f2ad0043039f0acdb9f316387c42c43
Md Mustafa Ali <md.ali@mustafass435.successivetech.com>
2025-06-06 01:18:08 +0530

    Update changes in assignment3

--- src/assignment3/question5.js
+++ src/assignment3/question5.js
@@ -1,17 +1,16 @@
 /* 5. Write a program to print all even number first and then all odd numbers using only one iteration */
 
-let number = 10;
+const number = 10;
 
-let even="";
-let odd="";
+let even = "";
+let odd = "";
 
-for(let i=1;i<=number;i++){
- 
-    if(i%2==0){
-        even+=i+" ";
-    }else{
-        odd+=i+" ";
-    }
+for(let i=1; i <= number; i++) {
+  if(i%2 === 0) {
+      even += i + " ";
+  }else {
+      odd += i + " ";
+  }
 }
 console.log(`All even numbers are : ${even}` );
 console.log(`All odd numbers are : ${odd}`);
--- src/assignment3/question7.js
+++ src/assignment3/question7.js
@@ -1,12 +1,12 @@
 /* 7. Write a program to display the twice of a number starting from 2 and end at 4096 (2, 4, 8, 16, ..... 4096) */
 
 
-let num=2;
-let res="";
+let num = 2;
+let res = "";
 
-while(num<=4096){
-    res+=num+" ";
-    num=2*num;
+while(num <= 4096) {
+  res += num + " ";
+  num = 2 * num;
 }
 
 console.log(res);
