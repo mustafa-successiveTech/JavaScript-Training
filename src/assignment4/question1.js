4e33c8b9f35315ca628bd73d7ce8ca5af4b63cee
Md Mustafa Ali <md.ali@mustafass435.successivetech.com>
2025-06-06 12:10:19 +0530

    Assignment4 Started

--- src/assignment4/question1.js
+++ src/assignment4/question1.js
@@ -3,7 +3,7 @@
 // Approach 1 - Using Set
 
 function removeDuplicate(str) {
-    return [...new Set(str)].join("");
+  return [...new Set(str)].join("");
 }
 
 console.log(removeDuplicate("Hello World"));
@@ -14,15 +14,16 @@ console.log(removeDuplicate("My name is naman"));
 let result = "";
 
 function removeDuplicateUsingIndex(str) {
-    for(let i=0; i < str.length; i++) {
-        if(result.indexOf(str[i]) === -1) {
-            result += str[i];
-        }
-    }
-    console.log(result);
+  for(let i=0; i < str.length; i++) {
+      if(result.indexOf(str[i]) === -1) {
+          result += str[i];
+      }
+  }
+  console.log(result);
 }
 
 removeDuplicateUsingIndex("Hello World");
+
 
 
 
